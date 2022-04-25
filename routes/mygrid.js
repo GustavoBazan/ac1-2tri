aa599ccad1255d20d93d785426c65115b738a79c
GustavoBazan <99882976+GustavoBazan@users.noreply.github.com>
2022-04-25 10:49:16 -0300

    Add files via upload

--- routes/mygrid.js
+++ routes/mygrid.js
@@ -34,4 +34,25 @@ module.exports = (app)=>{
             res.send('Não foi possível gravar os dados no DB')
         })
     })
+    //listar o documento para o excluir
+    app.get('/mygrid_excluir',async(req,res)=>{
+        //recuperar a id da barra de endereço
+        var id = req.query.id
+        //procurar o documento especifico
+        var procurar = await modelo.findOne({_id:id})
+        console.log(procurar)
+        //abrir a view mygrid_excluir e enviar a json do documento
+        res.render('mygrid_excluir.ejs',{dados:procurar})
+
+    })
+    //excluir documento da coleção atual
+    app.get('/excluir_mygrid',async(req,res)=>{
+        //recuperand o id da barra de endereços
+        var id = req.query.id
+        //excluindo o documento da coleção
+        var excluir = await modelo.findOneAndRemove({_id:id})
+        //voltar para a página mygrid
+        res.redirect('/mygrid')
+
+    })
 }
